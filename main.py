7f27ced662484b4a554d0ac20c945461fd5dace5
abhuuey77 <divyawadhwani087@gmail.com>
2026-02-06 13:11:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import threading
 
 WISHLIST_API = "https://www.sheinindia.in/api/wishlist/getwishlist"
 
-TELEGRAM_BOT_TOKEN = "8593087053:AAFHYMBPPEjqDZ2zPSP1KtM3Opsnc-SytNE" # Change with your telegram bot token
+TELEGRAM_BOT_TOKEN = "8558867781:AAHOlTebfnM0UJxXfAWX8HINdm_0kysXhhs" # Change with your telegram bot token
 TELEGRAM_CHAT_ID = "@abhiwhislistbot" # Change with your telegram chat id
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
